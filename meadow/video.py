889b310d5efa015926eed35ffba2ab8c98bdb0e8
achilles schmelzer <a@achilles.engineering>
2024-03-28 22:13:04 -0700

    Implement bare minimum video -> textract pipeline
    So I have the mount set up, and I have my phone in the mount, and I
    have a stream of video, and I have the AWS SDK, and it basically
    JUST WORKS. Textract seems to be actually quite good.
    
    The most squirrelly thing at the moment is the camera mount is kind of
    right up in my grill so that the full view is just a single page. I did
    this to avoid having to detect a page, pull it out of a frame, and then
    send that to Textract. That said, I feel like page detection on a dark
    desk should be a solved problem.
    
    The other thing that I have to think about a little bit is how to
    trigger the Textract API. I really want it to be automatic because the
    whole point is flow, and having to remember to press a button every time
    you get to a new page is not flow.
    
    So, do I want to get the mount and flow down or do I want to jump
    straight into the rest of the pipeline: parse the textract response,
    send it to OpenAI, and maybe trigger some things depending on the
    response from OpenAI?
    
    I think I'm going to implement the rest of the pipeline. Now that I know
    that I can definitely manage the raw video part of the project, I think
    I can just implement the rest of the pipeline using a stubbed image.
    That way this janky-ass mount and camera setup won't be a pain. Ok, so
    next session is:
    
    - parse the response
    - ask OpenAI a few questions about the parsed text
    - display a feed of the responses somewhere!

--- meadow/video.py
+++ meadow/video.py
@@ -1,24 +1,39 @@
 # import the opencv library 
 import cv2 
+import boto3
+import json
+
+textract = boto3.client('textract')
   
   
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
   
+has_sent = False
 while(True): 
-      
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read() 
-  
+
     # Display the resulting frame 
-    cv2.imshow('window', frame) 
+    cv2.imshow('window', cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE))
       
     # the 'q' button is set as the 
     # quitting button you may use any 
     # desired button of your choice 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
+
+    if has_sent == False and cv2.waitKey(1) & 0xFF == ord('c'): 
+        has_sent = True
+        print("Encoding image as png")
+        bytes = cv2.imencode('.png', frame)[1].tobytes()
+        print("Sending image to textract")
+        response = textract.detect_document_text(Document={'Bytes': bytes})
+        print("Response from textract received")
+        with open('./response.json', 'w') as f:
+            f.write(json.dumps(response, indent=4))
+        
   
 # After the loop release the cap object 
 vid.release() 
